ce680be36c1abd72abafd6b2acc9b29f4a0bf2f0
Trey Rader <treyrdr09@gmail.com>
2016-07-18 18:29:38 -0500

    audio function added

--- js/app.js
+++ js/app.js
@@ -8,12 +8,26 @@ $(document).ready(function() {
 		$('.ryu-still').show();;
   })
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-126px')
+			}
+		);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
