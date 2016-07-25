e16b1c3fb8e6363e9925e451582dc2f14dfe533c
Trey Rader <treyrdr09@gmail.com>
2016-07-25 16:01:25 -0500

    an entirely new js file/sound/satie.mp3 added, and modified index.html

--- js/app.js
+++ js/app.js
@@ -1,28 +1,40 @@
+ function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+};
 
+function playTakeFive() {
+	$('#take-five')[0].volume = 0.5;
+	$('#take-five')[0].load();
+	$('#take-five')[0].play();
+}
 
-
-
-
+function playSatie() {
+	$('#satie')[0].volume = 0.5;
+	$('#satie')[0].load();
+	$('#satie')[0].play();
+}
+ 
 
 $(document).ready(function() {
-	//$('#first-line').fadeTo("slow", 1);
-	$('.content').addClass('line1').fadeTo("slow", 1).text("Put the mouse on Ryu to get him ready for combat!");
-});
+	playTakeFive()
+	$('#instruction-1').show();
 	$('.ryu').mouseenter(function() {
+		$('#instruction-1').fadeTo("slow", 0, function() {
+			$('#instruction-2').fadeTo("slow", 1);
+		})
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-		
-		$('.line1').fadeTo("slow", 0, function() {
-			$(this).hide();
+	});
+	$('.ryu').mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+		$('#instruction-2').fadeTo("slow", 0, function() {
+			$('#instruction-1').fadeTo("slow", 1); 
 		})
-		$('.content').addClass('line2').fadeTo("slow", 1).text("Ok, Let's begin...");
-
 	})
-	.mouseleave(function() {
-    	$('.ryu-ready').hide();
-		$('.ryu-still').show();;
-  })
-	.mousedown(function() {
+	$('.ryu').mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
@@ -35,41 +47,34 @@ $(document).ready(function() {
 			}
 		);
 	})
-	.mouseup(function() {
+	$('.ryu').mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-	})
-
+		$('#instruction-2').fadeTo("slow", 0, function() {
+			$('#instruction-3').fadeTo("slow", 1);
+		})
+	});
 	$(this).keydown(function(e){
-       var code = e.keyCode; /*not entirely sure how this code is implemented*/
+       var code = e.keyCode; 
         if(code == 88){
          	$('.ryu-ready').hide();
          	$('.ryu-still').hide();
-         	$('.ryu-cool').show();   
+         	$('.ryu-cool').show();
+         	$('#instruction-3').fadeTo("slow", 0);    
+        	$('#take-five').trigger('pause');
+        	$('#satie').trigger('play');
+
         }
-    })
+    });
 	$(this).keyup(function(e){
        var code = e.keyCode;
         if(code == 88){
         	$('#take-five').hide();
          	$('.ryu-cool').hide();
          	$('.ryu-ready').show();
-        // $('.ryu-still').show();   
+         	$('#satie').trigger('pause');
+         	$('#take-five').trigger('play');
         }
-    }) 
-	playTakeFive();
+    }); 
 
-
-
-
-function playHadouken() {
-	$('#hadouken-sound')[0].volume = 0.5;
-	$('#hadouken-sound')[0].load();
-	$('#hadouken-sound')[0].play();
-}
-
-function playTakeFive() {
-	$('#take-five')[0].volume = 0.5;
-	$('#take-five')[0].load();
-	$('#take-five')[0].play();
-}
+});
